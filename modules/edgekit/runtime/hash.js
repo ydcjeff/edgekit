d03d988348cb50a92aa849626eff533729801d73
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-08-28 18:15:44 +0630

    feat: split fnv1a fn into string and buffer variants

--- modules/edgekit/runtime/hash.js
+++ modules/edgekit/runtime/hash.js
@@ -1,4 +1,4 @@
-const encoder = /* @__PURE__ */ new TextEncoder();
+const encoder = /* #__PURE__ */ new TextEncoder();
 
 /**
  * The SHA hash function.
@@ -18,28 +18,47 @@ export async function sha(data, algo = 'sha-1') {
 /**
  * The fnv-1a hash function took from https://deno.land/std/http/file_server.ts
  *
- * @param {string | Uint8Array} buf
+ * @param {string | Uint8Array} input
  */
-export function fnv1a(buf) {
-	let hash = 2166136261; // 32-bit FNV offset basis
-	if (typeof buf === 'string') {
-		for (let i = 0; i < buf.length; i++) {
-			hash ^= buf.charCodeAt(i);
-			// Equivalent to `hash *= 16777619` without using BigInt
-			// 32-bit FNV prime
-			hash +=
-				(hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
-		}
-	} else {
-		for (let i = 0; i < buf.length; i++) {
-			hash ^= buf[i];
-			// Equivalent to `hash *= 16777619` without using BigInt
-			// 32-bit FNV prime
-			hash +=
-				(hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
-		}
+export function fnv1a(input) {
+	if (typeof input === 'string') {
+		return fnv1a_str(input);
 	}
+	return fnv1a_buf(input);
+}
 
+/**
+ * The fnv-1a hash function took from https://deno.land/std/http/file_server.ts
+ *
+ * @param {string} str
+ */
+export function fnv1a_str(str) {
+	let hash = 2166136261; // 32-bit FNV offset basis
+	for (let i = 0; i < str.length; i++) {
+		hash ^= str.charCodeAt(i);
+		// Equivalent to `hash *= 16777619` without using BigInt
+		// 32-bit FNV prime
+		hash +=
+			(hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+	}
+	// 32-bit hex string
+	return (hash >>> 0).toString(16);
+}
+
+/**
+ * The fnv-1a hash function took from https://deno.land/std/http/file_server.ts
+ *
+ * @param {Uint8Array} buf
+ */
+export function fnv1a_buf(buf) {
+	let hash = 2166136261; // 32-bit FNV offset basis
+	for (let i = 0; i < buf.length; i++) {
+		hash ^= buf[i];
+		// Equivalent to `hash *= 16777619` without using BigInt
+		// 32-bit FNV prime
+		hash +=
+			(hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+	}
 	// 32-bit hex string
 	return (hash >>> 0).toString(16);
 }
